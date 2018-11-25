f2a0b1c9a776f90841479a7aa4ecbc36edcfa01b
Ryan Moore <rmoore8869@gmail.com>
2018-11-24 19:40:39 -0600

    scriptified demo.py

--- demo.py
+++ demo.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python3
-import gym
+import retro
+from subprocess import Popen, PIPE
 
 
-def run():
-    env = gym.make('MountainCar-v0')
+if __name__ == "__main__":
+    # rm TMNT video, if there is one
+    try:
+        tmnt_video_file = 'TeenageMutantNinjaTurtlesIIITheManhattanProject-Nes-1Player.Leo.Level1-000000.mp4'
+        rm_file_proc = Popen('rm ' + tmnt_video_file, shell=True, stdout=PIPE)
+        rm_file_proc.communicate()
+    except FileNotFoundError:
+        pass
+    # rm TMNT replay, if there is one
+    tmnt_bk_file = 'TeenageMutantNinjaTurtlesIIITheManhattanProject-Nes-1Player.Leo.Level1-000000.bk2'
+    rm_file_proc = Popen('rm ' + tmnt_bk_file, shell=True, stdout=PIPE)
+    rm_file_proc.communicate()
 
-    for episode in range(100):
-        env.reset()
-        state_pos = 0
-        while 1:
-            env.render()
+    env = retro.make(game='TeenageMutantNinjaTurtlesIIITheManhattanProject-Nes', record='.')
+    env.reset()
+    while 1:
+        sample_action = env.action_space.sample()
+        while(sample_action[1] == 1):
+            sample_action = env.action_space.sample()
+        _obs, _rew, done, _info = env.step(sample_action)
+        if done:
+            break
 
-            action = env.action_space.sample()
-            state_pos = env.step(2)[0][1]
-            if state_pos < 0:
-                state = env.step(1)
-            else:
-                state = env.step(2)
+    # create video file from replay
+    create_video_proc = Popen('python3 -m retro.scripts.playback_movie ' + tmnt_bk_file, shell=True, stdout=PIPE) 
+    create_video_proc.wait()
 
-            state_po = state[0][1]
+    # open up the replay
+    play_replay = Popen('open ' + tmnt_video_file, shell=True, stdout=PIPE)
+    play_replay.communicate()
 
-
-if __name__ == '__main__':
-    run()
