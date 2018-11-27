4ebdd61cf90ed6086c6d87384784a9e5785ed08b
Ryan Moore <ryan.moore@tagglogistics.com>
2018-11-26 19:47:58 -0600

    move convenience methods for file handling

--- demo.py
+++ demo.py
@@ -1,36 +1,59 @@
 #!/usr/bin/env python3
+from time import sleep
+
 import retro
-from subprocess import Popen, PIPE
+
+from file_handler import FileHandler
+
+
+game_name = 'TeenageMutantNinjaTurtlesIIITheManhattanProject-Nes'
+game_meta = '-1Player.Leo.Level1-000000'
+
+
+def make_game():
+    env = retro.make(game=game_name, record='.')
+    env.reset()
+    return env
+
+def make_video():
+    handler = FileHandler(game_name + game_meta)
+    handler.create_video()
+
+def play_video():
+    handler = FileHandler(game_name + game_meta)
+    handler.play_video()
 
 
 if __name__ == "__main__":
-    # rm TMNT video, if there is one
-    try:
-        tmnt_video_file = 'TeenageMutantNinjaTurtlesIIITheManhattanProject-Nes-1Player.Leo.Level1-000000.mp4'
-        rm_file_proc = Popen('rm ' + tmnt_video_file, shell=True, stdout=PIPE)
-        rm_file_proc.communicate()
-    except FileNotFoundError:
-        pass
-    # rm TMNT replay, if there is one
-    tmnt_bk_file = 'TeenageMutantNinjaTurtlesIIITheManhattanProject-Nes-1Player.Leo.Level1-000000.bk2'
-    rm_file_proc = Popen('rm ' + tmnt_bk_file, shell=True, stdout=PIPE)
-    rm_file_proc.communicate()
 
-    env = retro.make(game='TeenageMutantNinjaTurtlesIIITheManhattanProject-Nes', record='.')
+    game_name = 'TeenageMutantNinjaTurtlesIIITheManhattanProject-Nes'
+    game_meta = '-1Player.Leo.Level1-000000'
+
+    # create a file handlin object
+    handler = FileHandler(game_name + game_meta)
+
+    # rm TMNT video, if there is one
+    handler.rm_video()
+    # rm TMNT replay, if there is one
+    handler.rm_backup()
+
+    env = retro.make(game=game_name, record='.')
     env.reset()
     while 1:
         sample_action = env.action_space.sample()
-        while(sample_action[1] == 1):
+        while(sample_action[0] == 1 & sample_action[8] == 1):
             sample_action = env.action_space.sample()
+        print(env.buttons)
+        print(sample_action)
         _obs, _rew, done, _info = env.step(sample_action)
         if done:
             break
 
     # create video file from replay
-    create_video_proc = Popen('python3 -m retro.scripts.playback_movie ' + tmnt_bk_file, shell=True, stdout=PIPE) 
-    create_video_proc.wait()
+    handler.create_video()
 
-    # open up the replay
-    play_replay = Popen('open ' + tmnt_video_file, shell=True, stdout=PIPE)
-    play_replay.communicate()
+    sleep(10)
+
+    # spin it
+    handler.play_video()
 
---
+++ file_handler.py
@@ -0,0 +1,38 @@
+from subprocess import Popen
+from subprocess import PIPE
+
+
+class FileHandler:
+
+    def __init__(self, filename):
+        self.backup = filename + '.bk2'
+        self.video = filename + '.mp4'
+
+    def create_video(self):
+        # create video file from replay
+        create_video_proc = Popen('python3 -m retro.scripts.playback_movie ' + self.backup, shell=True, stdout=PIPE) 
+        create_video_proc.wait()
+
+    def rm_backup(self):
+        try:
+            rm_file_proc = Popen('rm ' + self.backup, shell=True, stdout=PIPE)
+            rm_file_proc.wait()
+        except FileNotFoundError:
+            pass
+
+    def rm_video(self):
+        try:
+            rm_file_proc = Popen('rm ' + self.video, shell=True, stdout=PIPE)
+            rm_file_proc.communicate()
+        except FileNotFoundError:
+            pass
+
+    def play_video(self):
+        
+        try:
+            play_replay = Popen('open ' + self.video, shell=True, stdout=PIPE)
+            play_replay.communicate()
+        except:
+            play_replay = Popen('xdg-open ' + self.video, shell=True, stdout=PIPE)
+            play_replay.communicate()
+
