9d58e99a37b4a2c2cfefcd5c1288549c264966cf
Ryan Moore <rmoore8869@gmail.com>
2018-12-11 03:39:38 -0600

    updating my updates

--- color.py
+++ color.py
@@ -1,10 +1,19 @@
+#!/usr/bin/env python3
+
 # rgb -> hex
-from Pillow import Image
+import numpy as np
+from PIL import Image
+import PIL
 
 def greyscale(r, g, b):
     return .2126 * r + .7152 * g + .0722 * b
 
 def get_img(name):
-    img = Image.read(name, 'r')
+    return Image.open(name, 'r')
 
 img = get_img('important_image.jpeg')
+
+im_arr = np.fromstring(img.tobytes(), dtype=np.uint8)
+
+im = PIL.Image.fromarray(im_arr)
+im.show()
