5fe7e4d1039f0a40b2124a827d8d7b3bf1828cc2
Ryan Moore <rmoore8869@gmail.com>
2018-12-19 00:08:26 -0600

    brule greyscale is a go

--- wavelet.py
+++ wavelet.py
@@ -3,12 +3,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-import pywt
 import cv2
-from pywt import dwt2, idwt2
+from pywt import dwt2
 
+img = cv2.imread('important_image.jpeg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cA, (cH, cV, cD) = pywt.dwt2(img, 'db1')
+cA, (cH, cV, cD) = dwt2(img, 'db1')
 
 fig = plt.figure(figsize=(12, 3))
 for i, a in enumerate([cA, cH, cV, cD]):
