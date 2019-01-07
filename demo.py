fbd0c6815c95a5d76d64535cbbe52410d7cb725a
Ryan Moore <ryan.moore@tagglogistics.com>
2019-01-06 22:21:09 -0600

    dumbbot works - need to get to beebop

--- demo.py
+++ demo.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+
+from random import randint
 from time import sleep
 
 import retro
-import numpy
+import numpy as np
 
 from file_handler import FileHandler
 from model import Model
@@ -10,6 +12,8 @@ from model import Model
 game_name = 'TeenageMutantNinjaTurtlesIIITheManhattanProject-Nes'
 game_meta = '-1Player.Leo.Level1-000000'
 
+
+data_file = open('gamedata.txt', 'w+')
 
 def make_game():
     env = retro.make(game=game_name, record='.')
@@ -24,6 +28,30 @@ def play_video():
     handler = FileHandler(game_name + game_meta)
     handler.play_video()
 
+
+# associate actions with 'cooldown' times, 
+# so that when an action is taken, 
+# we know when it is possible to take the next action
+# after that
+
+ACTION_SPACE = {
+    "b": 60,
+    "null": 60,
+    "select": 0,
+    "start": 0,
+    "up": 0,
+    "down": 0,
+    "left": 0,
+    "right": 0,
+    "a": 30
+}
+
+
+# get the action name of the button push array
+# returns strings like 'jump' or 'right' or some shit
+def get_action_name(pushed_index):
+    keys = ACTION_SPACE.keys()
+    return keys[pushed_index]
 
 if __name__ == "__main__":
 
@@ -41,17 +69,71 @@ if __name__ == "__main__":
     env = retro.make(game=game_name, record='.')
     env.reset()
 
-    f = open("moves.txt", "w+")
     forward = True
     done = 0
-    cumulative_reward = 0
-    i = 0
+    cooldown_time = 0
+
+    cooldown_timer = 0
+    pushed_index = 0
+    direction = 7
+
+    iterator = 0
+    attack_cooldown = 10
+    jump_cooldown = 10
+    
+    # file to write data for neural net
+    f = open('moves.txt', 'w+')
+
     while not done:
-        i += 1
-        sample_action = env.action_space.sample()
-        f.write(numpy.array2string(sample_action) + '\n')
+        attack_cooldown -= 1 
+        jump_cooldown -= 1 
+        iterator += 1
+
+        # default action is do nothing
+        sample_action = np.zeros(9, dtype=np.int8)
+        sample_action[direction] = 1
+
+        # switch from right to left
+        # 1/10 times
+        if direction == 7:
+            switch_direction = randint(0, 10)
+            if switch_direction == 1:
+                direction = 6 
+
+        # switch from left to right
+        # switch 1/5 times
+        if direction == 6:
+            switch_direction = randint(0, 5)
+            if switch_direction == 1:
+                direction = 7 
+
+        # wiggle up and down 1/3 of the time
+        wiggle = randint(0,3)
+        if wiggle == 1:
+            sample_action[5] = 1
+        elif wiggle == 2:
+            sample_action[4] = 1
+
+        # attack 1/46 times
+        # (roughly 2/3 of a second)
+        if iterator % 25 == 0 and jump_cooldown <= 0:
+            sample_action[0] = 1
+            # prevent special attacks
+            sample_action[8] = 0
+            attack_cooldown = 10
+
+        # jump 2/3 of a time each second and turtle hasnt attacked recently
+        if iterator % 21 == 0 and attack_cooldown <= 0:
+            sample_action[8] = 1
+            # prevent special attacks
+            sample_action[0] = 0
+            jump_cooldown = 10
+
         _obs, _rew, done, _info = env.step(sample_action)
-    print(i)
+        
+        output_data = np.append(sample_action, _rew)
+
+        data_file.write(str(output_data) + '\n')
 
     # create video file from replay
     #handler.create_video()
--- wavelet.py
+++ wavelet.py
@@ -10,20 +10,24 @@ from pywt import dwt2
 from pywt._extensions._pywt import Wavelet
 
 img = cv2.imread('important_image.jpeg')
+img2 = np.asarray(img)
+
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-print(img[0])
-print(len(img))
- 
+
+dft_array = np.fft.fft(img2) 
+freq = np.fft.fftfreq(img2.shape[-1])
+
+print(freq)
+print(dft_array.shape)
+
 c = math.sqrt(2)/2
 dec_lo, dec_hi, rec_lo, rec_hi = [1, -c], [3000, c], [c, c], [-c, c]
+#dec_lo, dec_hi, rec_lo, rec_hi = [c for x in range(1000)], [-c for x in range(1000)], [c for x in range(1000)], [-c for x in range(1000)]
 
 filter_bank = [dec_lo, dec_hi, rec_lo, rec_hi]
 my_wavelet = pywt.Wavelet(name="myHaarWavelet", filter_bank=filter_bank)
 cA, (cH, cV, cD) = dwt2(img, my_wavelet)
-
-#brule_mother_wavelet = Wavelet('', [cA, cH, cV, cD])
-#print(brule_mother_wavelet)
 
 fig = plt.figure(figsize=(12, 3))
 for i, a in enumerate([cA, cH, cV, cD]):
