521fc32604603341c39c3a324ca2b43e652c6f13
Ryan Moore <rmoore8869@gmail.com>
2019-01-19 16:15:16 -0600

    some genetic stuff updates

--- genetic_test.py
+++ genetic_test.py
@@ -2,9 +2,10 @@
 
 import random
 
-from deap import tools
-from deap import creator
+from deap import algorithms
 from deap import base
+from deap import creator
+from deap import tools
 
 creator.create("StuffMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.StuffMax)
@@ -13,8 +14,29 @@ toolbox = base.Toolbox()
 
 # Attribute generator
 toolbox.register("attr_bool", random.randint, 0, 1)
-
 # Structure initializers
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, 100)
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n=100)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+def evalOneMax(individual):
+    return sum(individual),
+
+toolbox.register("evaluate", evalOneMax)
+toolbox.register("mate", tools.cxTwoPoint)
+toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+toolbox.register("select", tools.selTournament, tournsize=3)
+
+population = toolbox.population(n=300)
+
+
+NGEN=40
+for gen in range(NGEN):
+    offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.1)
+    fits = toolbox.map(toolbox.evaluate, offspring)
+    for fit, ind in zip(fits, offspring):
+        ind.fitness.values = fit
+        print(fit, ind)
+    population = toolbox.select(offspring, k=len(population))
+
+top10 = tools.selBest(population, k=10)
 
