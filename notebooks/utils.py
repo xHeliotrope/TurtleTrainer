3442f9065cdcabe3f16fc8eea4ffb40e223a905f
Ryan Moore <rmoore8869@gmail.com>
2019-02-17 00:16:56 -0600

    fixed score listing function

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,10 +3,10 @@ import re
 
 def list_all_scores():
     scores = {num: [] for num in range(20)}
-    for (root, dirs, files) in walk('recordings'):
+    for (root, dirs, files) in walk('../recordings'):
         if('stats.txt' in files and len(files) == 3):
-            gen = re.search('/(.*)/', root).group(1)
-            scores[gen].append(int(float(files[0])))
+            gen = re.search('recordings/(.*)/', root).group(1)
+            scores[int(gen)].append(int(float(files[0])))
 
     return scores
 
