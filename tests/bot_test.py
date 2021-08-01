76f8213d15cad979a8eb540bd17f3e688dd2d5fb
Ryan Moore <rmoore8869@gmail.com>
2021-08-01 02:16:46 -0500

    use unittest classes for testing

--- tests/bot_test.py
+++ tests/bot_test.py
@@ -1,21 +1,27 @@
+import unittest
+
 import retro
 
 from trainer.handler import FileHandler
 from trainer.genetic.genetic import game_name
 from trainer.model import Bot
 
-def test_create_filehandler():
-    fh = FileHandler()
-    assert type(fh) == FileHandler
 
-def test_create_env():
-    fh = FileHandler()
-    env = retro.make(game=game_name, record='./' + fh.root_path)
-    assert type(env) == retro.retro.RetroEnv
+class ModuleTest(unittest.TestCase):
+    """The basic classes/functions of the module work as expected
+    """
+    def test_create_filehandler(self):
+        fh = FileHandler()
+        assert type(fh) == FileHandler
 
-def test_create_bot():
-    fh = FileHandler()
-    env = retro.make(game=game_name, record='./' + fh.root_path)
-    reward = 0
-    bot = Bot(env, reward, fh)
-    assert type(bot) == Bot
+    def test_create_env(self):
+        fh = FileHandler()
+        env = retro.make(game=game_name, record='./' + fh.root_path)
+        assert type(env) == retro.retro.RetroEnv
+
+    def test_create_bot(self):
+        fh = FileHandler()
+        env = retro.make(game=game_name, record='./' + fh.root_path)
+        reward = 0
+        bot = Bot(env, reward, fh)
+        assert type(bot) == Bot
